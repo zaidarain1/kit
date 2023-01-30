f71dd67e81f399c634d6523063db1ce32f503d42
sagostinelli <batonenbois@gmail.com>
2023-01-30 15:19:19 -0500

    injected wallet show logo and hide existing similar wallet

--- packages/core/src/utils/getEthConnectWallets.ts
+++ packages/core/src/utils/getEthConnectWallets.ts
@@ -2,9 +2,11 @@ import React from 'react'
 import { Connector } from 'wagmi'
 
 export interface WalletProperties {
+  id: string,
   logo: React.FunctionComponent,
   name: string,
   iconBackground?: string,
+  hideConnectorId?: string | null
 }
 
 export type Wallet = WalletProperties & {
@@ -20,7 +22,17 @@ export type ExtendedConnector = Connector & WalletField
 export const getEthConnectWallets = (wallets: Wallet[]) => {
   const connectors: Connector[] = []
 
-  wallets.forEach(wallet => {
+  // hide connector if there is an identical injected wallet
+  const injectedWallet = wallets.find(connector => connector.id === 'injected-wallet')
+
+  const filteredWallets = wallets.filter((wallet) => {
+    if (!injectedWallet) {
+      return true
+    }
+    return wallet.id !== injectedWallet.hideConnectorId
+  })
+
+  filteredWallets.forEach(wallet => {
     const { createConnector, ...metaProperties } = wallet
     const connector = wallet.createConnector()
     const walletProperties = { ...metaProperties }
--- packages/wallets/src/wallets/injectedWallet/injectedWallet.ts
+++ packages/wallets/src/wallets/injectedWallet/injectedWallet.ts
@@ -1,23 +1,69 @@
+import React from 'react'
+import { ethers } from 'ethers'
 import { Chain } from 'wagmi/chains'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 
 import { InjectedWalletLogo } from './InjectedWalletLogo'
+import { MetamaskWalletLogo } from '../metamaskWallet/MetamaskWalletLogo'
+import { SequenceWalletLogo } from '../sequenceWallet/SequenceWalletLogo'
+
+export interface Window {
+  ethereum?: ethers.providers.ExternalProvider;
+}
+
+export interface MoreExternalProviders {
+  isSequence?: boolean
+}
 
 export interface InjectedWalletOptions {
   chains: Chain[];
   shimDisconnect?: boolean;
 }
 
-export const injectedWallet = ({ chains, shimDisconnect }: InjectedWalletOptions) => ({
-  id: 'injected-wallet',
-  logo: InjectedWalletLogo,
-  // iconBackground: '#fff',
-  name: 'Injected Wallet',
-  createConnector: () => {
-    const connector = new InjectedConnector({
-      chains,
-      options: { shimDisconnect }
-    });
-    return connector
+export interface InjectedWalletInformation {
+  hideConnectorId: string | null;
+  name: string;
+  logo: React.FunctionComponent
+}
+
+const getInjectedWalletInformation = (): InjectedWalletInformation => {
+  if (window?.ethereum?.isMetaMask) {
+    return {
+      name: 'Metamask',
+      logo: MetamaskWalletLogo,
+      hideConnectorId: 'metamask'
+    }
+  /* @ts-ignore-next-line */
+  } else if (window?.ethereum?.isSequence) {
+    return {
+      name: 'Sequence',
+      logo: SequenceWalletLogo,
+      hideConnectorId: 'sequence'
+    }
+  } else {
+    return {
+      name: 'Injected Wallet',
+      logo: InjectedWalletLogo,
+      hideConnectorId: null
+    }
   }
-})
+}
+
+export const injectedWallet = ({ chains, shimDisconnect }: InjectedWalletOptions) => {
+  const { name, hideConnectorId, logo } = getInjectedWalletInformation()
+
+  return ({
+    id: 'injected-wallet',
+    hideConnectorId,
+    logo,
+    // iconBackground: '#fff',
+    name,
+    createConnector: () => {
+      const connector = new InjectedConnector({
+        chains,
+        options: { shimDisconnect }
+      });
+      return connector
+    }
+  })
+}
