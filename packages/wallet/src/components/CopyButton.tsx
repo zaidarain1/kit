47c459d4965324498e379571b51a4bb45f773613
sagostinelli <batonenbois@gmail.com>
2023-05-03 17:02:11 -0400

    display address

---
+++ packages/wallet/src/components/CopyButton.tsx
@@ -0,0 +1,32 @@
+import { Button, CopyIcon, CheckmarkIcon } from '@0xsequence/design-system'
+import React, { useEffect, useState, ComponentProps } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+type ButtonProps = ComponentProps<typeof Button>
+
+interface CopyButtonProps extends ButtonProps {
+  text: string
+}
+
+export const CopyButton = (props: CopyButtonProps) => {
+  const { text, size = 'xs', ...rest } = props
+  const [isCopied, setCopy] = useState(false)
+
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => {
+        setCopy(false)
+      }, 4000)
+    }
+  }, [isCopied])
+
+  const handleCopy = () => {
+    setCopy(true)
+  }
+
+  return (
+    <CopyToClipboard text={text} onCopy={handleCopy}>
+      <Button size={size} leftIcon={isCopied ? CheckmarkIcon : CopyIcon} label={isCopied ? 'Copied' : 'Copy'} {...rest} />
+    </CopyToClipboard>
+  )
+}
--- packages/wallet/src/components/SequenceConnectWalletProvider.tsx
+++ packages/wallet/src/components/SequenceConnectWalletProvider.tsx
@@ -4,11 +4,15 @@ import {
   Box,
   Text,
   ThemeProvider,
+  GradientAvatar
 } from '@0xsequence/design-system'
 import { AnimatePresence } from 'framer-motion'
+import { useAccount } from 'wagmi'
+import { truncateAtMiddle } from '../utils/helpers'
+
+import { CopyButton  } from './CopyButton'
 
 import { WalletModalContext } from '../contexts'
-
 
 import '@0xsequence/design-system/styles.css'
 
@@ -26,6 +30,7 @@ export type SequenceConnectWalletProvider = {
 
 export const SequenceConnectWalletProvider = ({ children, theme }: SequenceConnectWalletProvider) => {
   const [openWalletModal, setOpenWalletModal] = useState<boolean>(false)
+  const { address } = useAccount()
 
   return (
     <WalletModalContext.Provider value={{ setOpenWalletModal }}>
@@ -35,7 +40,17 @@ export const SequenceConnectWalletProvider = ({ children, theme }: SequenceConne
             <Modal scroll={false} backdropColor="transparent" size="sm" onClose={() => setOpenWalletModal(false)}>
               <Box paddingY="16" paddingX="8">
                 <Box justifyContent="center" alignItems="center"><Text variant="xlarge">My Wallet</Text></Box>
-                 Welcome, ADDRESS + ICON
+                  <Box paddingTop="2" alignItems="center" cursor="pointer">
+                    <GradientAvatar address={address || ''} />
+                    <Text marginLeft="2">
+                      {truncateAtMiddle(address || '', 12)}
+                    </Text>
+                    <CopyButton text={address || ''} />
+                  </Box>
+              </Box>
+              <Box>USD$123</Box>
+              <Box>
+                Coins
               </Box>
             </Modal>
           )}
