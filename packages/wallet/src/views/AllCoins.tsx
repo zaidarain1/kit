bac875f04fcd2c7c6d379b35557b2fd81956cbfd
sagostinelli <batonenbois@gmail.com>
2023-05-05 16:57:59 -0400

    added allcoins view

--- packages/wallet/src/views/AllCoins.tsx
+++ packages/wallet/src/views/AllCoins.tsx
@@ -1,10 +1,27 @@
+import { Box, Button, Text, vars } from '@0xsequence/design-system'
+import { ethers } from 'ethers'
 import React from 'react'
-import { Box, Button } from '@0xsequence/design-system'
+import { useAccount, useNetwork } from 'wagmi'
 
-import { useNavigation } from '../hooks/useNavigation'
+import { CoinRow, CoinRowSkeleton } from '../components/CoinRow'
+
+import { getNativeTokenInfoByChainId } from '../utils'
+import { useBalances, useNavigation } from '../hooks'
+
 
 export const AllCoins = () => {
+  const { address } = useAccount()
+  const { chain } = useNetwork()
+  const chainId = chain?.id || 1
   const setNavigation = useNavigation()
+
+  const { data: balances, isLoading } = useBalances({ accountAddress: address || '', chainId })
+
+  const nativeTokenInfo = getNativeTokenInfoByChainId(chainId)
+
+  const nativeToken = balances?.find(b => b.contractAddress === ethers.constants.AddressZero)
+
+  const erc20Tokens = balances?.filter(b => b.contractType === 'ERC20')
 
   const onClickBack = () => {
     setNavigation && setNavigation({
@@ -12,12 +29,56 @@ export const AllCoins = () => {
     })
   }
 
+  const getContent = () => {
+    if(isLoading) {
+      return (
+        <Box width="full" flexDirection="column" gap="2">
+          <CoinRowSkeleton />
+          <CoinRowSkeleton />
+        </Box>
+      )
+    }
+
+    return (
+      <Box>
+        <Box marginBottom="1">
+          <Text variant="small" color="text50">{`Coins (${totalTokens})`}</Text>
+        </Box>
+        <Box width="full" flexDirection="column" gap="2">
+          {nativeToken && (
+            <CoinRow
+              imageUrl={nativeTokenInfo.logoURI}
+              decimals={nativeTokenInfo.decimals}
+              name={nativeTokenInfo.name}
+              symbol={nativeTokenInfo.symbol}
+              balance={nativeToken.balance} 
+            />
+          )}
+          {erc20Tokens && (
+            erc20Tokens.map(token => (
+              <CoinRow
+                imageUrl={token.contractInfo?.logoURI || ''}
+                decimals={token.contractInfo?.decimals || 18}
+                name={token.contractInfo?.name || ''}
+                symbol={token.contractInfo?.symbol || ''}
+                balance={token.balance} 
+              />
+            ))
+          )}
+        </Box>
+      </Box>
+    )
+  }
+
+  const totalTokens = 1 + (erc20Tokens?.length || 0)
+
   return (
     <Box paddingX="4" paddingBottom="4">
       <Box alignItems="center" style={{ height: '60px' }}>
         <Button onClick={onClickBack} label="back" />
       </Box>
-      all coins go here
+
+      {getContent()}
     </Box>
   )
 }
