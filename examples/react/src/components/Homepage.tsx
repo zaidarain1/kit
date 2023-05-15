8b4a0daeb292cbaba7921fe54a92efaea8c47d3b
SamueleA <batonenbois@gmail.com>
2023-05-15 16:57:29 -0400

    added args to paper call

--- examples/react/src/components/Homepage.tsx
+++ examples/react/src/components/Homepage.tsx
@@ -62,17 +62,80 @@ function Homepage() {
     }
   }
 
+  const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256[]",
+          "name": "_tokenIds",
+          "type": "uint256[]"
+        },
+        {
+          "internalType": "uint256[]",
+          "name": "_tokensBoughtAmounts",
+          "type": "uint256[]"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_maxCurrency",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_deadline",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address",
+          "name": "_recipient",
+          "type": "address"
+        },
+        {
+          "internalType": "address[]",
+          "name": "_extraFeeRecipients",
+          "type": "address[]"
+        },
+        {
+          "internalType": "uint256[]",
+          "name": "_extraFeeAmounts",
+          "type": "uint256[]"
+        }
+      ],
+      "name": "buyTokens",
+      "outputs": [
+        {
+          "internalType": "uint256[]",
+          "name": "",
+          "type": "uint256[]"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+
   const getCheckoutSettings = () => {
+    const deadline = String(Math.round(new Date().getTime()/ 1000) + 3600)
+
     const checkoutSettings: CheckoutSettings = {
       chainId: 137,
-      receiptTitle: 'Custom Sequence Kit Checkout',
-      abi: 'abi',
+      receiptTitle: 'Custom Sequence Kit Checkout Demo',
+      abi: JSON.stringify(abi),
       methodName: 'buyTokens',
-      methodArguments: {},
+      methodArguments: {
+        "_recipient":"$WALLET",
+        "_deadline": deadline,
+        "_maxCurrency":"12",
+        "_tokenIds":["65000"],
+        "_tokensBoughtAmounts":["100"],
+        "_extraFeeRecipients":[],
+        "_extraFeeAmounts":[]
+      },
       recipientAddress: address || '',
-      contractAddress: '0x967c902d4ea61b167fc56b27a36a546b266ac0fb',
+      contractAddress: '0x8bb759bb68995343ff1e9d57ac85ff5c5fb79334',
       currency: 'USDC',
       currencyAmount: '0.1',
+      collectionContractAddress: '0x631998e91476da5b870d741192fc5cbc55f5a52e',
       onSuccess: () => { console.log('success') },
       onError: () => { console.log('error') },
     }
