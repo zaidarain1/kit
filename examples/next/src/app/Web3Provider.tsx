bd512ddec1e9838dfe9cac9552e4bf8465414f36
Corban Riley <corbanbrook@gmail.com>
2024-04-25 14:36:55 -0400

    Move kitConfig to config

--- examples/next/src/app/Web3Provider.tsx
+++ examples/next/src/app/Web3Provider.tsx
@@ -4,45 +4,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
 // import { KitProvider } from '@0xsequence/kit'
 
-import { config } from './config'
+import { wagmiConfig /*, kitConfig */ } from './config'
 
 const queryClient = new QueryClient()
-
-// const kitConfig: KitConfig = {
-//   defaultTheme: 'dark',
-//   signIn: {
-//     projectName: 'Kit Demo'
-//     // logoUrl: 'sw-logo-white.svg',
-//     // useMock: isDebugMode
-//   },
-//   displayedAssets: [
-//     // Native token
-//     {
-//       contractAddress: '0x0000000000000000000000000000000000000000',
-//       chainId: 42170
-//     },
-//     // Native token
-//     {
-//       contractAddress: '0x0000000000000000000000000000000000000000',
-//       chainId: 421614
-//     },
-//     // Waas demo NFT
-//     {
-//       contractAddress: '0x0d402c63cae0200f0723b3e6fa0914627a48462e',
-//       chainId: 42170
-//     },
-//     // Waas demo NFT
-//     {
-//       contractAddress: '0x0d402c63cae0200f0723b3e6fa0914627a48462e',
-//       chainId: 421614
-//     },
-//     // Skyweaver assets
-//     {
-//       contractAddress: '0x631998e91476da5b870d741192fc5cbc55f5a52e',
-//       chainId: 137
-//     }
-//   ]
-// }
 
 export interface Web3ProviderProps {
   children: React.ReactNode
@@ -52,7 +16,7 @@ export const Web3Provider = (props: Web3ProviderProps) => {
   const { children } = props
 
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         {/* <KitProvider config={kitConfig}>{children}</KitProvider> */}
         {children}
--- examples/next/src/app/config.ts
+++ examples/next/src/app/config.ts
@@ -5,6 +5,7 @@ import { CreateConnectorFn, createConfig, http } from 'wagmi'
 import { Transport } from 'viem'
 import { arbitrumNova, arbitrumSepolia, mainnet, polygon } from 'viem/chains'
 import { getDefaultConnectors } from '@0xsequence/kit-connectors'
+// import { KitConfig } from '@0xsequence/kit'
 
 const walletConnectProjectId = 'c65a6cb1aa83c4e24500130f23a437d8'
 const projectAccessKey = 'AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI'
@@ -36,9 +37,45 @@ const connectors: CreateConnectorFn[] = [
   //   : [])
 ]
 
-export const config = createConfig({
+export const wagmiConfig = createConfig({
   chains,
   transports,
   connectors,
   ssr: true // For Next.js SSR support
 })
+
+// export const kitConfig: KitConfig = {
+//   defaultTheme: 'dark',
+//   signIn: {
+//     projectName: 'Kit Demo'
+//     // logoUrl: 'sw-logo-white.svg',
+//     // useMock: isDebugMode
+//   },
+//   displayedAssets: [
+//     // Native token
+//     {
+//       contractAddress: '0x0000000000000000000000000000000000000000',
+//       chainId: 42170
+//     },
+//     // Native token
+//     {
+//       contractAddress: '0x0000000000000000000000000000000000000000',
+//       chainId: 421614
+//     },
+//     // Waas demo NFT
+//     {
+//       contractAddress: '0x0d402c63cae0200f0723b3e6fa0914627a48462e',
+//       chainId: 42170
+//     },
+//     // Waas demo NFT
+//     {
+//       contractAddress: '0x0d402c63cae0200f0723b3e6fa0914627a48462e',
+//       chainId: 421614
+//     },
+//     // Skyweaver assets
+//     {
+//       contractAddress: '0x631998e91476da5b870d741192fc5cbc55f5a52e',
+//       chainId: 137
+//     }
+//   ]
+// }
