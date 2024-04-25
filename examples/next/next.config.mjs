288902ffa059fa1b96917bcdc98140a7a57bae0d
Corban Riley <corbanbrook@gmail.com>
2024-04-25 15:38:52 -0400

    externalize pino-pretty and encoding in next config

--- examples/next/next.config.mjs
+++ examples/next/next.config.mjs
@@ -4,6 +4,10 @@ const withVanillaExtract = createVanillaExtractPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: config => {
+    config.externals.push('pino-pretty', 'encoding')
+    return config
+  }
   // transpilePackages: ['@0xsequence/kit', '@0xsequence/kit-wallet', '@0xsequence/kit-connectors', '@0xsequence/checkout']
 }
 
