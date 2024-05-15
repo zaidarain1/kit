4ec0a825ee10c14eaadb3d8c363fce4f66e745e5
Corban Riley <corbanbrook@gmail.com>
2024-05-14 21:23:27 -0400

    Removing comments

--- examples/next/src/app/Providers.tsx
+++ examples/next/src/app/Providers.tsx
@@ -13,12 +13,10 @@ const queryClient = new QueryClient()
 export interface ProvidersProps {
   initialState: State | undefined
   children: React.ReactNode
-  // wagmiConfig: Config
-  // kitConfig: KitConfig
 }
 
 export const Providers = (props: ProvidersProps) => {
-  const { initialState, children /* , wagmiConfig, kitConfig */ } = props
+  const { initialState, children } = props
 
   return (
     <WagmiProvider config={wagmiConfig} initialState={initialState}>
