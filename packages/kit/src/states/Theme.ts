765ef1ac5dc673aed3dc35ab3a460b6b6f2854e0
samuelea <batonenbois@gmail.com>
2024-05-16 15:13:44 -0400

    clean up

--- packages/kit/src/states/Theme.ts
+++ packages/kit/src/states/Theme.ts
@@ -1,19 +1,3 @@
-// import React from 'react'
-
-// import { ModalPosition, Theme } from '../index'
-
-// import { createGenericContext } from './genericContext'
-
-// type ThemeContext = {
-//   setTheme: React.Dispatch<React.SetStateAction<Theme>>
-//   theme: Theme
-//   position: ModalPosition
-//   setPosition: React.Dispatch<React.SetStateAction<ModalPosition>>
-// }
-
-// export const [useThemeContext, ThemeContextProvider] = createGenericContext<ThemeContext>()
-
-
 import { proxy } from "valtio";
 
 import { ModalPosition, Theme } from '../index'
