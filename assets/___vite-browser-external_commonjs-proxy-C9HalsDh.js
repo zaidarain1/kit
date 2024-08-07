0c2d04809941811a6516fd0f239f76374f736f9d
corbanbrook <corbanbrook@gmail.com>
2024-08-07 08:54:04 +0000

    Build: (80a4008) Fix Text and Token Styling (#113)
    * Fix BalanceItem
    
    * Fixing WalletLink text style
    
    * Fixing Coin details page
    
    * Fixing text variants

--- assets/___vite-browser-external_commonjs-proxy-C9HalsDh.js
+++ assets/___vite-browser-external_commonjs-proxy-C9HalsDh.js
@@ -1,4 +1,4 @@
-import { g as getDefaultExportFromCjs, a as getAugmentedNamespace } from "./index-C52aQQpt.js";
+import { g as getDefaultExportFromCjs, a as getAugmentedNamespace } from "./index-BF4hWsGJ.js";
 var events = { exports: {} };
 var R = typeof Reflect === "object" ? Reflect : null;
 var ReflectApply = R && typeof R.apply === "function" ? R.apply : function ReflectApply2(target, receiver, args) {
--- assets/hooks.module-B6I_3ieY.js
+++ assets/hooks.module-B6I_3ieY.js
@@ -1,5 +1,5 @@
-import { r as require$$2$1, p as process$1, c as commonjsGlobal, i as inherits_browserExports, B as Buffer$7, a as getAugmentedNamespace } from "./index-C52aQQpt.js";
-import { e as eventsExports, r as require$$3 } from "./___vite-browser-external_commonjs-proxy-DJj0alCw.js";
+import { r as require$$2$1, p as process$1, c as commonjsGlobal, i as inherits_browserExports, B as Buffer$7, a as getAugmentedNamespace } from "./index-BF4hWsGJ.js";
+import { e as eventsExports, r as require$$3 } from "./___vite-browser-external_commonjs-proxy-C9HalsDh.js";
 var readableBrowser = { exports: {} };
 var streamBrowser = eventsExports.EventEmitter;
 var buffer_list;
