85fc2cfb160b7091f87f103715efc4f824c3d405
samuelea <batonenbois@gmail.com>
2024-08-14 10:45:12 -0400

    comment

--- packages/checkout/src/views/PendingTransaction.tsx
+++ packages/checkout/src/views/PendingTransaction.tsx
@@ -41,6 +41,7 @@ export const PendingTransaction = () => {
 
   const authToken = data?.token
 
+  // using prod endpoints just for testing and debugging...
   const url = !isDev
   ? `https://sardine-checkout-sandbox.sequence.info?api_url=https://sardine-api-sandbox.sequence.info&client_token=${authToken}&show_features=true`
   : `https://sardine-checkout.sequence.info?api_url=https://sardine-api.sequence.info&client_token=${authToken}&show_features=true`
