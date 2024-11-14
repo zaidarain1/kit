ad432c2dae89278f74d17867f222221d538e1581
samuelea <batonenbois@gmail.com>
2024-11-14 10:49:13 -0500

    sardine-currency-abstraction

--- examples/react/src/components/Connected.tsx
+++ examples/react/src/components/Connected.tsx
@@ -331,10 +331,16 @@ export const Connected = () => {
     // const price = '200000000000000'
 
     // // ERC-20 contract
-    const currencyAddress = '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359'
-    const salesContractAddress = '0xe65b75eb7c58ffc0bf0e671d64d0e1c6cd0d3e5b'
-    const collectionAddress = '0xdeb398f41ccd290ee5114df7e498cf04fac916cb'
-    const price = '20000'
+    // const currencyAddress = '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359'
+    // const salesContractAddress = '0xe65b75eb7c58ffc0bf0e671d64d0e1c6cd0d3e5b'
+    // const collectionAddress = '0xdeb398f41ccd290ee5114df7e498cf04fac916cb'
+    // const price = '20000'
+
+    // // ERC-20 currency abstraction for sardine
+    const currencyAddress = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
+    const salesContractAddress = '0x04e1bad2dbe48991d4a239ee8f4f24cc5ada83a8'
+    const collectionAddress = '0x3c36713a5a742f567821899d0c7cd3480e9bacd6'
+    const price = '20000000000000'
 
     const chainId = 137
 
